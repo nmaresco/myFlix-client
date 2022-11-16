3d58aee3252ac53bc47fbed773dd0ca9cc53ddd2
nmaresco <nicole.maresco@gmail.com>
2022-11-15 17:16:36 -0700

    Added props and structs

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,17 +1,39 @@
 import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component
 {
-
+    constructor(){
+        super();
+        this.state = {
+            movies: [],
+            selectedMovie: null
+        }
+    }
+    setSelectedMovie(newSelectedMovie) {
+      this.setState({
+        selectedMovie: newSelectedMovie
+      });
+    }
     render() {
-        return (
-            <div className='main-view'>
-                <div>Inception</div>
-                <div>The Shawshank Redemption</div>
-                <div>Gladiator</div>
-            </div>
-        );
+      const { movies, selectedMovie } = this.state;
+  
+  
+      if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+  
+      return (
+        <div className="main-view">
+          {selectedMovie
+            ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+            : movies.map(movie => (
+              <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+            ))
+          }
+        </div>
+      );
     }
 }
+
 
 export default MainView;
---
+++ src/components/movie-card/movie-card.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+
+export class MovieCard extends React.Component {
+    render() {
+        const { movie, onMovieClick } = this.props;
+    
+        return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
+      }
+    }
